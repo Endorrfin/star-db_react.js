229f83d74ef8ee357731ef8083998b6ae9b02c6c
Vasyl Krupka <krupka.ua@gmail.com>
2020-01-09 11:05:00 +0200

    Updates

---
+++ precache-manifest.5db14e650b00ae2d79efe89ee8890490.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09a63d6152ab619696ffdecda59308f0",
+    "url": "/star-db_react.js/index.html"
+  },
+  {
+    "revision": "3ed4f9768e70ac8c66f7",
+    "url": "/star-db_react.js/static/css/main.375db9b4.chunk.css"
+  },
+  {
+    "revision": "1946bca2f5fafe2a113c",
+    "url": "/star-db_react.js/static/js/2.ec519e28.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/star-db_react.js/static/js/2.ec519e28.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3ed4f9768e70ac8c66f7",
+    "url": "/star-db_react.js/static/js/main.213d7c86.chunk.js"
+  },
+  {
+    "revision": "18f64ce0680cef2bb39c",
+    "url": "/star-db_react.js/static/js/runtime-main.597de0ae.js"
+  }
+]);
